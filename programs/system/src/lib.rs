c9a99a29ab15d993bdd9e095ed4793b5776f8ff6
Flavio B <redmannequin@users.noreply.github.com>
2025-07-27 16:55:56 -0400

    define CanInvoke

--- programs/system/src/lib.rs
+++ programs/system/src/lib.rs
@@ -1,5 +1,46 @@
 #![no_std]
 
+use pinocchio::{
+    account_info::AccountInfo,
+    instruction::{AccountMeta, Instruction, Signer},
+    pubkey::Pubkey,
+    ProgramResult,
+};
+
 pub mod instructions;
 
 pinocchio_pubkey::declare_id!("11111111111111111111111111111111");
+
+pub trait CanInvoke<const ACCOUNTS_LEN: usize> {
+    fn invoke_via(
+        &self,
+        invoke: impl FnOnce(
+            /* program_id: */ &Pubkey,
+            /* accounts: */ &[&AccountInfo; ACCOUNTS_LEN],
+            /* account_metas: */ &[AccountMeta],
+            /* data: */ &[u8],
+        ) -> ProgramResult,
+    ) -> ProgramResult;
+
+    fn invoke(&self) -> ProgramResult {
+        self.invoke_via(|program_id, accounts, account_metas, data| {
+            let instruction = Instruction {
+                program_id: program_id,
+                accounts: &account_metas,
+                data: data,
+            };
+            pinocchio::cpi::invoke(&instruction, accounts)
+        })
+    }
+
+    fn invoke_signed(&self, signers: &[Signer]) -> ProgramResult {
+        self.invoke_via(|program_id, accounts, account_metas, data| {
+            let instruction = Instruction {
+                program_id: program_id,
+                accounts: &account_metas,
+                data: data,
+            };
+            pinocchio::cpi::invoke_signed(&instruction, accounts, signers)
+        })
+    }
+}
